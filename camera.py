9083462729d199e2be57cd56fb0aac82c0fa66a3
akii4552 <109889734+akii4552@users.noreply.github.com>
2022-07-24 12:49:40 +0530

    Add files via upload

---
+++ camera.py
@@ -0,0 +1,21 @@
+# import cv2
+# import streamlit as st
+
+# st.title("Webcam Application")
+# run = st.checkbox('Run')
+# FRAME_WINDOW = st.image([])
+# cam = cv2.VideoCapture(0)
+
+# while run:
+#     ret, frame = cam.read()
+#     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#     FRAME_WINDOW.image(frame)
+# else:
+#     st.write('Stopped')
+import streamlit as st
+from streamlit_webrtc import webrtc_streamer
+
+st.title("My first Streamlit app")
+st.write("Hello, world")
+
+webrtc_streamer(key="example")
